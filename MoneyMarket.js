847aa97664dbaa95028e7347d9304308cbe80ea8
Kufre Godwin <79638255+kelmorgan@users.noreply.github.com>
2021-03-01 09:57:27 +0100

    Update MoneyMarket.js

--- MoneyMarket.js
+++ MoneyMarket.js
@@ -1,15 +1,20 @@
-funtion formLoadMM () {
-alert('code frim git js repo');
+function formLoadMM() {
 }
 
-function customValidation (op){
-    switch (op){
-        case "S":{}
-        case "I": {}
-        case "D": {}
-    }
+function customValidation(op){
+     switch (op) {
+        case 'S':
+        break;
+        case 'I':{}
+		break;  
+        case 'D':{}
+		break;    
+        default:
+            break;
+    }	
+    return true;
 }
 
-function gSelectMarket(){
-    executeServerEvent('onSelectMarket','onChnage','',true);
+function gSelectMarket (){
+	executeServerEvent('onChangeMarket','onChange','',true);
 }
