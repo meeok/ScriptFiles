09564eae4c1d85649764cac27e3f4fbae43c4b40
Kufre Godwin <79638255+kelmorgan@users.noreply.github.com>
2021-03-26 17:49:43 +0100

    Add files via upload
    done

--- MoneyMarket.js
+++ MoneyMarket.js
@@ -1,20 +1,64 @@
+
 function formLoadMM() {
 }
 
 function customValidation(op){
      switch (op) {
-        case 'S':
+        case 'S':{}
         break;
         case 'I':{}
 		break;  
-        case 'D':{}
+        case 'D':{} 
 		break;    
         default:
             break;
     }	
+        setDecisionHistory();
+        sendMail();
     return true;
 }
 
-function gSelectMarket (){
-	executeServerEvent('onChangeMarket','onChange','',true);
+function selectProcess (){
+	executeServerEvent('onChangeProcess','onChange','',true);
+}
+
+function setDecisionHistory (){
+    var decHisFlag = getValue("g_decisionHistoryFlag");
+    if (decHisFlag == null || decHisFlag == "")
+        executeServerEvent("","decisionHistory","",true);
+}
+
+function sendMail (){
+    executeServerEvent('','sendMail','',true);
+}
+
+function goBackToDashboard(){
+    executeServerEvent('onClickGoBackToDashboard','onClick','',true);
+}
+
+function cpSelectCategory(){
+    executeServerEvent('onChangeCategory','onChange','',true);
+}
+
+function cpUpdateLandMsg(){
+    executeServerEvent('onClickUpdateMsg','onClick','',true);
+}
+
+function setupWin(){
+   var resp = executeServerEvent('setupWin','onClick','',true);
+   if (resp == "Window setup successful"){
+        showMessage("",resp,"confirm");
+        completeWorkItem();
+   }
+   else   showMessage("",resp,"confirm");
+}
+
+function cpSubmitlandMsgUpdate(){
+    var updateMsgflag = getValue('cp_updateMsg');
+    if (updateMsgflag == "true")
+          completeWorkItem();
+}
+
+function isEmpty(resp){
+    return resp == null || resp == "";
 }
